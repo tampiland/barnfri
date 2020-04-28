ed041d1eca56e60ec93131e436d84d1b5884fcf7
Peter Karlsson <peterk@petes-macbook-pro.local>
2020-04-28 11:39:54 +0200

    Edited layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import { useState } from "react";
 import "./App.css";
 import MyDate from "./MyDate";
 
+const todayDate = new MyDate();
+const maxDate = new MyDate();
+maxDate.setDate(maxDate.getDate() + 365);
+
 function App() {
   const [evalDate, setEvalDate] = useState<MyDate>(new MyDate());
 
@@ -16,13 +20,18 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <input
-          type='date'
-          value={evalDate.toLocaleDateString()}
-          onChange={handleChange}
-        />
         {getTitle(evalDate)}
-        {getTable(evalDate)}
+        {getTable(evalDate, todayDate)}
+        <p>&nbsp;</p>
+        <p>
+          <input
+            type='date'
+            min={todayDate.toLocaleDateString()}
+            max={maxDate.toLocaleDateString()}
+            value={evalDate.toLocaleDateString()}
+            onChange={handleChange}
+          />
+        </p>
       </header>
     </div>
   );
@@ -30,10 +39,10 @@ function App() {
 
 function getTitle(date: MyDate): JSX.Element {
   const title = date.getBarnfri() ? "Barnfri!" : "Inte barnfri!";
-  return <h2>{title}</h2>;
+  return <h1>{title}</h1>;
 }
 
-function getTable(date: MyDate): JSX.Element {
+function getTable(date: MyDate, today: MyDate): JSX.Element {
   const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5];
   const surrDates = array.map(
     (diff) => new MyDate(date.valueOf() + diff * 1000 * 3600 * 24)
@@ -43,14 +52,13 @@ function getTable(date: MyDate): JSX.Element {
     <tr key={d.toLocaleDateString()} className={"today"}>
       <td
         className={`date ${
-          d.toLocaleString() === date.toLocaleString() ? "today " : "nähä "
+          d.toLocaleDateString() === today.toLocaleDateString() ? "today " : ""
+        } ${
+          d.toLocaleDateString() === date.toLocaleDateString() ? "eval " : ""
         }`}>
         {d.toLocaleDateString()}
       </td>
-      <td
-        className={`weekday ${
-          d.toLocaleString() === date.toLocaleString() ? "today " : "nähä "
-        }${d.getBarnfri() ? "childfree" : "withchild"}`}>
+      <td className={`weekday ${d.getBarnfri() ? "childfree" : "withchild"}`}>
         {d.toWeekdayString()}
       </td>
     </tr>
