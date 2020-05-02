45d23d3a07c0026eb0931a8b24e5ea1ea7ff8556
Peter Karlsson <peterk@petes-mbp.lan>
2020-05-02 13:58:49 +0200

    Modified date formatting

--- src/Components/DatePicker.tsx
+++ src/Components/DatePicker.tsx
@@ -11,18 +11,18 @@ interface DatePickerProps {
 
 function DatePicker(props: DatePickerProps) {
   //const gray = "#e2e3e5";
+  const options = {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
 
   return (
     <Container>
       <Row className='justify-content-center align-items-center'>
         <Col xs={"6"} sm={"8"} className='p-1'>
-          <FormControl
-            className='text-center p-0'
-            type='text'
-            disabled
-            value={props.evalDate.toLocaleDateString("sv-SE")}
-            onChange={props.onChange}
-          />
+          {props.evalDate.toLocaleDateString("sv-SE", options)}
         </Col>
         <Col xs={"auto"} className='p-1'>
           <Button
